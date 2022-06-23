b2861bc93824fdb0d7de9724b821165b3449dee5
oceanCB <1956699404@qq.com>
2022-06-23 14:45:00 +0800

    update

--- pictoday.py
+++ pictoday.py
@@ -12,13 +12,17 @@ def readmecontent():
 <div style="display:flex;justify-content: center; align-items:center;">
 <h1>   欢迎来到CB的博客网站 👋</h1>
 </div>
+
  
-## Picture of the day
+<h2> Picture of the day</h2>
+
 
 <div style="display:flex;justify-content: center; align-items:center;">
-    <img width=400px src="{wiki_link}">
+    <img width=400px src="{wiki_link}"/>
 </div>
-    
+
+
+
       
 > {wiki_content}
   """
