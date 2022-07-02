f963f624c979101070d7fd24c0b20b0027b99a8a
oceanCB <1956699404@qq.com>
2022-07-02 18:22:05 +0800

    update

--- pictoday.py
+++ pictoday.py
@@ -27,34 +27,31 @@ def readmecontent():
 > {wiki_content}
   """
 
-    # print("download image...")
-    # content = requests.get(URL).content
-    # html = lxml.html.fromstring(content)
-    # presentation_table = html.xpath("//table[@role='presentation']")[0]
-    # a_tag = presentation_table.xpath(".//a[@class='image']")[0]
-    # relative_link = a_tag.get("href")
-    # title = a_tag.get("title")
-    # image_src = a_tag.xpath("./img/@srcset")[0]
-    # best_image = image_src.split(" ")[0]
-    # print(f"{relative_link} {title}, image srcset:{image_src}")
-    # print(f"best image: {best_image}")
+    print("download image...")
+    content = requests.get(URL).content
+    html = lxml.html.fromstring(content)
+    presentation_table = html.xpath("//table[@role='presentation']")[0]
+    a_tag = presentation_table.xpath(".//a[@class='image']")[0]
+    relative_link = a_tag.get("href")
+    title = a_tag.get("title")
+    image_src = a_tag.xpath("./img/@srcset")[0]
+    best_image = image_src.split(" ")[0]
+    print(f"{relative_link} {title}, image srcset:{image_src}")
+    print(f"best image: {best_image}")
 
-    # content = requests.get(URL).content
-    # html = lxml.html.fromstring(content)
-    # presentation_table = html.xpath("//table[@role='presentation']")[0]
-    # content = presentation_table.xpath('.//tbody/tr/td')[1]
-    # ptext = content.xpath('.//p')[0]
+    content = requests.get(URL).content
+    html = lxml.html.fromstring(content)
+    presentation_table = html.xpath("//table[@role='presentation']")[0]
+    content = presentation_table.xpath('.//tbody/tr/td')[1]
+    ptext = content.xpath('.//p')[0]
 
-    # contenttext = ptext.xpath('.//text()')
+    contenttext = ptext.xpath('.//text()')
 
-    # new_readme = README.format(
-    #     wiki_link="https://" + best_image[2:],
-    #     wiki_content=' '.join(contenttext)
-    # )
     new_readme = README.format(
-        wiki_link="https://",
-        wiki_content='dsafsdf'
+        wiki_link="https://" + best_image[2:],
+        wiki_content=' '.join(contenttext)
     )
+
     return new_readme
 
 
--- 微软机器学习系统课本/README.md
+++ 微软机器学习系统课本/README.md
@@ -9,11 +9,20 @@
 
 
 <div style="display:flex;justify-content: center; align-items:center;">
+<<<<<<< HEAD
     <img width=400px src="https://"/>
+=======
+    <img width=400px src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Mar%C3%A9chal_Canrobert_by_Nadar.jpg/450px-Mar%C3%A9chal_Canrobert_by_Nadar.jpg"/>
+>>>>>>> fb75b5e0e1d7a2e574156f6005bc1ff355d231b7
 </div>
 
 
 
       
+<<<<<<< HEAD
 > dsafsdf
+=======
+> François Certain de Canrobert  (27 June 1809 – 28 January 1895) was a French general who held the title of  Marshal of France . A staunch supporter of Emperor  Napoleon III , he became one of the leading figures in the  Bonapartist  party under the  French Third Republic . This photograph of Canrobert in military uniform was taken by the French photographer  Nadar .
+
+>>>>>>> fb75b5e0e1d7a2e574156f6005bc1ff355d231b7
   
